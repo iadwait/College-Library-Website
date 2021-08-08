c482915fc47765e20a15da44fb6f86549791c49f
iadwait <adwaitbarkale@gmail.com>
2021-08-08 10:47:05 +0530

    Displayed Books

--- JS/index.js
+++ JS/index.js
@@ -1,48 +1,71 @@
 console.log("This is JS File for College Library Project");
 
 // Create Book Constructor
-function Book(name,author,type) {
+function Book(name, author, type) {
     this.name = name;
     this.author = author;
     this.type = type;
 }
 
 // Display Constructor
-function display() {
+function Display() {
 
 }
 
-// Add Method to display prototype
+// Add Method to Display prototype
+Display.prototype.add = function (book) {
+    let tableBody = document.getElementById('bookDetailsTableBody');
+    let tableBodyHTML = `
+        <tr>
+            <th scope="row">1</th>
+            <td>${book.name}</td>
+            <td>${book.author}</td>
+            <td>${book.type}</td>
+         </tr>
+    `
+    tableBody.innerHTML += tableBodyHTML;
+}
+Display.prototype.clear = function () {
+    // Reset Library Form Fields
+    let libraryForm = document.getElementById('libraryForm');
+    libraryForm.reset(); // reset will erase/reset all form fields
+}
+
 
 // Add Submit Event Listener to libraryForm
 let libraryForm = document.getElementById('libraryForm');
-libraryForm.addEventListener('submit',libraryFormSubmit);
+libraryForm.addEventListener('submit', libraryFormSubmit);
 
 function libraryFormSubmit(e) {
     console.log("Form Submitted");
     // Get Values for Name Author and Type
-    let name = document.getElementById('bookName').value; 
-    let author = document.getElementById('bookAuthor').value; 
-    
+    let name = document.getElementById('bookName').value;
+    let author = document.getElementById('bookAuthor').value;
+
     // Programming Learning Scientist - Radio buttons ID
-    let programming = document.getElementById('Programming'); 
-    let learning = document.getElementById('Learning'); 
+    let programming = document.getElementById('Programming');
+    let learning = document.getElementById('Learning');
     let scientist = document.getElementById('Scientist');
     let type;
 
     // Check which checkBox is Checked
     if (programming.checked) {
         type = programming.value;
-    } else if(learning.checked) {
+    } else if (learning.checked) {
         type = learning.value;
-    } else if(scientist.checked) {
+    } else if (scientist.checked) {
         type = scientist.value;
     }
     // console.log("Name = " + name + " Author = " + author + " type = " + type);
-    
+
     // Create Book Object
-    let book = new Book(name,author,type);
-    console.log(typeof book);
+    let book = new Book(name, author, type);
+    // console.log(typeof book);
+
+    // Display Book on UI
+    let display = new Display();
+    display.add(book);
+    display.clear();
 
     // When any form is sbumitted page is reloaded, In order to avoid reload use preventDefault()
     e.preventDefault();
